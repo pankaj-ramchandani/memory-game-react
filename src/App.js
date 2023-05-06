a0d6d1c1ca18be76c3fd3044648e6c7d4c8e4a16
pankaj-ramchandani <ramchandanipankaj787@gmail.com>
2023-05-06 13:42:41 +0530

    finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Images from "./Images";
+import { shuffle } from "lodash";
 
 function App() {
+  const [cards, setCards] = useState(shuffle([...Images, ...Images]));
+  const [clicks, setClicks] = useState(0);
+  const [won, setWon] = useState(true);
+  const [activeCards, setActiveCards] = useState([]);
+  const [foundPairs, setFoundPairs] = useState([]);
+  function flipCard(index) {
+    if (won) {
+      setCards(shuffle([...Images, ...Images]));
+      setFoundPairs([]);
+      setWon(false);
+      setClicks(0);
+    }
+    if (activeCards.length === 0) {
+      setActiveCards([index]);
+    }
+    if (activeCards.length === 1) {
+      const firstIndex = activeCards[0];
+      const secondIndex = index;
+
+      if (cards[firstIndex] === cards[secondIndex]) {
+        setFoundPairs([...foundPairs, firstIndex, secondIndex]);
+        if (foundPairs.length + 2 === cards.length) setWon(true);
+      }
+      setActiveCards([...activeCards, index]);
+    }
+    if (activeCards.length == 2) {
+      setActiveCards([index]);
+    }
+    setClicks(clicks + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="board">
+        {cards.map((card, index) => {
+          const flippedToFront =
+            activeCards.indexOf(index) !== -1 ||
+            foundPairs.indexOf(index) !== -1
+              ? "flipped"
+              : "";
+          return (
+            <div
+              className={"card-outer " + flippedToFront}
+              onClick={() => flipCard(index)}
+            >
+              <div className="card">
+                <div className="front">
+                  <img src={card} alt="" />
+                </div>
+                <div className="back"></div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="stats">
+        {won && (
+          <>
+            You won the game ! Congratulations !<br />
+          </>
+        )}
+        Click: {clicks} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Found Pairs:{" "}
+        {foundPairs.length / 2}
+      </div>
     </div>
   );
 }
